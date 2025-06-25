5a1cf5e37121e5fc29ae1eac072edf43cc5d057b
fatihcvs <105765934+fatihcvs@users.noreply.github.com>
2025-06-26 00:39:26 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 document.addEventListener('DOMContentLoaded', async function() {
-    if (typeof ethers === 'undefined') { return; }
+    if (typeof ethers === 'undefined' || typeof L === 'undefined') return;
     const bscscanApiKey = 'YMWFRRRGXZFBF47SMRCQFMMDD9E9TYTSWX';
     const ledgerContractAddress = '0xd34f98A99F313781a3F463ff151f721cFB1bE448';
     const eventSignature = 'MemoryAnchored(address,uint256,string)';
     const eventTopic = ethers.utils.id(eventSignature);
     const apiUrl = `https://api.bscscan.com/api?module=logs&action=getLogs&fromBlock=0&toBlock=latest&address=${ledgerContractAddress}&topic0=${eventTopic}&apikey=${bscscanApiKey}`;
     var map = L.map('map').setView([45, 15], 2);
-    L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
-        attribution: '&copy; OpenStreetMap &copy; CARTO'
-    }).addTo(map);
+    L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', { attribution: '&copy; OpenStreetMap &copy; CARTO' }).addTo(map);
     try {
         const response = await fetch(apiUrl);
         const data = await response.json();
@@ -24,8 +22,7 @@ document.addEventListener('DOMContentLoaded', async function() {
                         const lon = parseFloat(decodedMessage.split('LON:')[1].split(';')[0]);
                         const message = decodedMessage.split('MSG:')[1].trim();
                         if (!isNaN(lat) && !isNaN(lon)) {
-                            const popupContent = `<b>Message:</b> ${message}<br><br><a href="https://bscscan.com/tx/${log.transactionHash}" target="_blank">View Transaction</a>`;
-                            L.marker([lat, lon]).addTo(map).bindPopup(popupContent);
+                            L.marker([lat, lon]).addTo(map).bindPopup(`<b>Message:</b> ${message}<br><br><a href="https://bscscan.com/tx/${log.transactionHash}" target="_blank">View Transaction</a>`);
                         }
                     } catch (e) {}
                 }
